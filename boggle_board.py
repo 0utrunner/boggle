da9701dc0b06c5822b5041e8960f7b4ac79a47e3
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-03 00:02:32 -0500

    kinda SOLVED

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,23 @@
+import string
+import random
+
 class BoggleBoard:
   
   def __init__(self):
-  
+    print(f'____\n____\n____\n____')
 
   def shake(self):
+    board = (f'____\n____\n____\n____')
+    alphabet_string = string.ascii_uppercase
+    alphabet_list = list(alphabet_string)
+    for i in range(len(board)):
+      board = (board.replace('_', (random.choice(alphabet_list)), 1))
+      
+    print(board)
+
+case = BoggleBoard()
+
+case.shake()
+  
+
+#Mayan Nubering System
