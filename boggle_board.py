cafdf1f9adbc0a76173e38907a88174bde7f3bfd
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-03 09:54:19 -0500

    Trying smart boggle

--- boggle_board.py
+++ boggle_board.py
@@ -8,6 +8,7 @@ class BoggleBoard:
 
   def shake(self):
     board = (f'_ _ _ _\n_ _ _ _\n_ _ _ _\n_ _ _ _')
+    # die = [['A','A','E','E','G','N'],['E','L','R','T','T','Y'],['A','O','O','T','T','W'],['A','B','B','J','O','O'],['E','H','R','T','V','W'],['C','I','M','O','T','U'],['D','I','S','T','T','Y'],['E','I','O','S','S','T'],['D','E','L','R','V','Y'],['A','C','H','O','P','S'],['H','I','M','N','Qu','U'],['E','E','I','N','S','U'],['E','E','G','H','N','W'],['A','F','F','K','P','S'],['H','L','N','N','R','Z'],['D','E','I','L','R','X']]
     alphabet_string = string.ascii_uppercase
     alphabet_list = list(alphabet_string)
     for i in range(len(board)):
@@ -18,3 +19,6 @@ class BoggleBoard:
 case = BoggleBoard()
 
 case.shake()
+
+
+
